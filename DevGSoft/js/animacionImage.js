b628503a3beb41bba4c3f789045c404583e841b9
morales <franciscomoralessanchez00@gmail.com>
2024-11-19 19:21:09 -0600

    agregando animaciones a las imagens y logo

---
+++ DevGSoft/js/animacionImage.js
@@ -0,0 +1,32 @@
+// Función para detectar si un elemento está en el viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Función para agregar o eliminar la clase "visible" cuando la imagen entra o sale del viewport
+function handleScroll() {
+  const images = document.querySelectorAll(".about__img"); // Seleccionamos todas las imágenes con la clase .about__img
+
+  images.forEach((image) => {
+    if (isInViewport(image)) {
+      // Si la imagen entra en el viewport, agregamos la clase "visible"
+      image.classList.add("about__img--visible");
+    } else {
+      // Si la imagen ya no está en el viewport, eliminamos la clase "visible"
+      image.classList.remove("about__img--visible");
+    }
+  });
+}
+
+// Ejecutamos la función handleScroll cuando el usuario haga scroll
+window.addEventListener("scroll", handleScroll);
+
+// Ejecutamos handleScroll también al cargar la página, por si la imagen ya está visible
+window.addEventListener("load", handleScroll);
