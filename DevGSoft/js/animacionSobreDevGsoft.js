35e2a5f3eac487a1e3b52da8c9768d47ab125631
morales <franciscomoralessanchez00@gmail.com>
2025-01-09 18:23:03 -0600

    agregando animacion en la seccion de sobre devgsoft

---
+++ DevGSoft/js/animacionSobreDevGsoft.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById("lottie-animation"), // El contenedor donde se cargará la animación
+  renderer: "svg", // Usamos SVG para una renderización más eficiente
+  loop: true, // La animación se repetirá de forma infinita
+  autoplay: true, // Inicia automáticamente cuando se carga
+  path: "recursos/Animation - 1736463928557.json", // Ruta al archivo .json de la animación
+});
