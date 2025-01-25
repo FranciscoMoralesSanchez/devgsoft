2a5ef72c1ffe533b7606cab6c612f74135e0a44a
morales <franciscomoralessanchez00@gmail.com>
2025-01-25 12:26:45 -0600

    actualizando

--- DevGSoft/js/scriptForm.js
+++ DevGSoft/js/scriptForm.js
@@ -1,15 +1,17 @@
 // Obtener el enlace de contacto y el formulario
-const contactLink = document.querySelector('.nav__items--cta'); // El enlace dentro del nav
-const contactForm = document.getElementById('contactForm'); // El contenedor del formulario flotante
-const closeFormButton = document.getElementById('closeForm'); // El botón "Cerrar" dentro del formulario
+const contactLink = document.querySelector(".nav__items--cta"); // El enlace dentro del nav
+const contactForm = document.getElementById("contactForm"); // El contenedor del formulario flotante
+const closeFormButton = document.getElementById("closeForm"); // El botón "Cerrar" dentro del formulario
+const form = document.querySelector(".contact-form"); // El formulario de contacto
 
 // Mostrar el formulario cuando se haga clic en el enlace
-contactLink.addEventListener('click', (e) => {
-    e.preventDefault(); // Prevenir que el enlace navegue
-    contactForm.style.display = 'flex'; // Mostrar el formulario flotante
+contactLink.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevenir que el enlace navegue
+  contactForm.style.display = "flex"; // Mostrar el formulario flotante
 });
 
 // Cerrar el formulario cuando se haga clic en el botón "Cerrar"
-closeFormButton.addEventListener('click', () => {
-    contactForm.style.display = 'none'; // Ocultar el formulario flotante
+closeFormButton.addEventListener("click", () => {
+  form.reset();
+  contactForm.style.display = "none"; // Ocultar el formulario flotante
 });
